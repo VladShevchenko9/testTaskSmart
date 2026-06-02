68e4febc5691d18d61c2d161eb27db19abe84564
VladShevchenko9 <vlad9031999@gmail.com>
2026-06-02 22:39:37 +0300

    updated models: scopes and docBlocks

--- src/app/Models/Ticket.php
+++ src/app/Models/Ticket.php
@@ -5,12 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
+/**
+ * @property int $id
+ * @property int $customer_id
+ * @property string $subject
+ * @property string $message
+ * @property string $status
+ * @property Carbon|null $manager_reply_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $customer
+ */
 class Ticket extends Model implements HasMedia
 {
-    use InteractsWithMedia, HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = [
         'customer_id',
@@ -24,8 +37,32 @@ class Ticket extends Model implements HasMedia
         'manager_reply_at' => 'datetime',
     ];
 
+    /**
+     * @return BelongsTo<User, $this>
+     */
     public function customer(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @param Builder<Ticket> $query
+     * @param Carbon $from
+     * @param Carbon $to
+     * @return Builder<Ticket>
+     */
+    public function scopeCreatedBetween(Builder $query, Carbon $from, Carbon $to): Builder
+    {
+        return $query->whereBetween('created_at', [$from, $to]);
+    }
+
+    /**
+     * @param Builder<Ticket> $query
+     * @param Carbon $from
+     * @return Builder<Ticket>
+     */
+    public function scopeCreatedFrom(Builder $query, Carbon $from): Builder
+    {
+        return $query->where('created_at', '>=', $from);
     }
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,19 +2,31 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+use Spatie\Permission\Traits\HasRoles;
 
-
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $phone
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Ticket> $tickets
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -51,9 +63,11 @@ class User extends Authenticatable
         ];
     }
 
+    /**
+     * @return HasMany<Ticket, $this>
+     */
     public function tickets(): HasMany
     {
         return $this->hasMany(Ticket::class);
     }
-    
 }
